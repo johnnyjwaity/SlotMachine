d3d827e1bea053661a000e8715d9f77fc8c03cb6
RC211 <rc211@d211.org>
2018-08-31 13:58:22 -0500

    First Android Syudio

---
+++ slotMachine/app/src/main/java/com/johnnywaity/adntest2/slotmachine/singleplayer.java
@@ -0,0 +1,13 @@
+package com.johnnywaity.adntest2.slotmachine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class singleplayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singleplayer);
+    }
+}
