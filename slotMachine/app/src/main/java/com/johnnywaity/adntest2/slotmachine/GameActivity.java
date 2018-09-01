f0df8a3936fa65b290f9a2eacecf62b973428678
Viraj Sule <virajsule12@gmail.com>
2018-09-01 17:02:03 -0500

    scrolling images almost done

--- slotMachine/app/src/main/java/com/johnnywaity/adntest2/slotmachine/GameActivity.java
+++ slotMachine/app/src/main/java/com/johnnywaity/adntest2/slotmachine/GameActivity.java
@@ -9,34 +9,137 @@ import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.GestureDetector.OnGestureListener;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GameActivity extends AppCompatActivity implements OnGestureListener {
 
     GestureDetector gestureDetector;
 //test comment
 
-//    final ImageView lever = (ImageView) findViewById(R.id.imageView);
-//    final ImageView topslot1 = (ImageView) findViewById(R.id.topslot1);
-//    final ImageView topslot2 = (ImageView) findViewById(R.id.topslot2);
-//    final ImageView topslot3 = (ImageView) findViewById(R.id.topslot3);
-//    final ImageView midslot1 = (ImageView) findViewById(R.id.midslot1);
-//    final ImageView midslot2 = (ImageView) findViewById(R.id.midslot2);
-//    final ImageView midslot3 = (ImageView) findViewById(R.id.midslot3);
-//    final ImageView botslot1 = (ImageView) findViewById(R.id.botslot1);
-//    final ImageView botslot2 = (ImageView) findViewById(R.id.botslot2);
-//    final ImageView botslot3 = (ImageView) findViewById(R.id.botslot3);
     private ArrayList<Integer> imgArray = new ArrayList<>();
+    private int[][] dataArray = new int[3][3];
+    private boolean killFirstCol = false;
+    private boolean killSecondCol = false;
+    private boolean killThirdCol = false;
+    private boolean slowDown = false;
+    private int imgTime = 100;
 
-    private void scrollImages(int res1,int res2, int res3){
+    private void killFC(){
+        killFirstCol = true;
+    }
 
+    private void killSC(){
+        killSecondCol = true;
+    }
+
+    private void killTC(){
+        killThirdCol = true;
+    }
+
+    private void scrollImages() {
+        System.out.println("yo");
+
+        final ImageView lever = (ImageView) findViewById(R.id.imageView);
+        final ImageView topslot1 = (ImageView) findViewById(R.id.topslot1);
+        final ImageView topslot2 = (ImageView) findViewById(R.id.topslot2);
+        final ImageView topslot3 = (ImageView) findViewById(R.id.topslot3);
+        final ImageView midslot1 = (ImageView) findViewById(R.id.midslot1);
+        final ImageView midslot2 = (ImageView) findViewById(R.id.midslot2);
+        final ImageView midslot3 = (ImageView) findViewById(R.id.midslot3);
+        final ImageView botslot1 = (ImageView) findViewById(R.id.botslot1);
+        final ImageView botslot2 = (ImageView) findViewById(R.id.botslot2);
+        final ImageView botslot3 = (ImageView) findViewById(R.id.botslot3);
+
+
+        topslot1.post(new Runnable() {
+            int i =2;
+            @Override
+            public void run() {
+                topslot1.setImageResource(imgArray.get(i));
+                i++;
+                if (i>5){
+                    i=0;
+                }
+                if (!killFirstCol){
+                    topslot1.postDelayed(this,imgTime);
+                }
+
+            }
+        });
+
+        midslot1.post(new Runnable() {
+            int i =1;
+            @Override
+            public void run() {
+                midslot1.setImageResource(imgArray.get(i));
+                i++;
+                if (i>5){
+                    i=0;
+                }
+                if(!killFirstCol){
+                    midslot1.postDelayed(this,imgTime);
+                }
+                if (slowDown){
+                    imgTime = 500;
+                    if (i==2){
+                        killFC();
+                    }
+                }
+
+            }
+        });
+
+        botslot1.post(new Runnable() {
+            int i =0;
+            @Override
+            public void run() {
+                botslot1.setImageResource(imgArray.get(i));
+                i++;
+                if (i>5){
+                    i=0;
+                }
+                if (!killFirstCol){
+                    botslot1.postDelayed(this,imgTime);
+                }
+
+            }
+        });
+
+        new CountDownTimer(5000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                slowDown = true;
+            }
+        }.start();
+
+
+
+    }
+
+    private void handleActivate(){
+        Button activatebtn = (Button) findViewById(R.id.activate);
+
+        activatebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scrollImages();
+            }
+        });
     }
 
 
@@ -51,8 +154,6 @@ public class GameActivity extends AppCompatActivity implements OnGestureListener
         imgArray.add(R.drawable.img5);
         imgArray.add(R.drawable.img6);
 
-        System.out.println(imgArray);
-
         final ImageView lever = (ImageView) findViewById(R.id.imageView);
         final ImageView topslot1 = (ImageView) findViewById(R.id.topslot1);
         final ImageView topslot2 = (ImageView) findViewById(R.id.topslot2);
@@ -63,6 +164,38 @@ public class GameActivity extends AppCompatActivity implements OnGestureListener
         final ImageView botslot1 = (ImageView) findViewById(R.id.botslot1);
         final ImageView botslot2 = (ImageView) findViewById(R.id.botslot2);
         final ImageView botslot3 = (ImageView) findViewById(R.id.botslot3);
+
+
+
+
+
+        dataArray[0][0] = 2;
+        dataArray[0][1] = 2;
+        dataArray[0][2] = 2;
+        dataArray[1][0] = 1;
+        dataArray[1][1] = 1;
+        dataArray[1][2] = 1;
+        dataArray[2][0] = 0;
+        dataArray[2][1] = 0;
+        dataArray[2][2] = 0;
+
+//        for (int i=0;i<dataArray.length;i++){
+//            for (int j=0;j<dataArray[i].length;i++){
+//                System.out.println();
+//            }
+//        }
+
+        handleActivate();
+
+        for(int i = 0; i<3; i++)
+        {
+            for(int j = 0; j<3; j++)
+            {
+                System.out.print(dataArray[i][j]);
+            }
+            System.out.println();
+        }
+
 
 //        topslot1.setImageDrawable(imgArray.get(0));
 
