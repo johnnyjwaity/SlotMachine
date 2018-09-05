c94c6b4b443d04658597a6f7541034987edfea80
RC211 <rc211@d211.org>
2018-09-05 13:51:21 -0500

    GamePlay

--- slotMachine/app/src/main/java/gameplay/GamePlay.java
+++ slotMachine/app/src/main/java/gameplay/GamePlay.java
@@ -2,4 +2,29 @@ package gameplay;
 
 public class GamePlay {
 
+    public double chance(double payout){ //payout is the player's bet
+        int chance = 0;
+        chance = (int)(Math.random()*30)+1;
+        if (chance>=1 && chance<=3){ //small jackpot, larger payout
+            return payout * 1.5;
+        }
+        else if ((chance>3) && (chance<=8)){ //smaller chance, moderate payout
+            return payout * 1.2;
+        }
+        else if (chance>8 && chance<15){ //larger chance, small payout
+            return payout * 1.1;
+        }
+        else if (chance>=15 && chance<=20){ //larger chance, small payout
+            return payout * 1.1;
+        }
+        else if (chance>20 && chance<29){ //no extra payout, largest chance
+            return payout;
+        }
+        else if (chance>=29 && chance<=30){ //very small chance, much larger payout
+            return payout * 2.5;
+        }
+        else { //shouldn't be needed but just in case
+            return payout;
+        }
+    }
 }
